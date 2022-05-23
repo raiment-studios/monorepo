ef46dbcc790fcb5b6b62f5de4f32c6e62708255b
Ridley Winters <65878718+ridleywinters@users.noreply.github.com>
2022-05-23 14:42:58 +0000

    update

---
+++ source/lib/engine/examples/10-sprite/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ReadingFrame } from '@raiment/react-ex';
+
+export default function () {
+    return (
+        <ReadingFrame>
+            <h1>Sprite</h1>
+            <div>IMAGE GOES HERE</div>
+        </ReadingFrame>
+    );
+}
