fd9f47346b7ad77e844d88298a9d866aebf3fd02
Ridley Winters <65878718+ridleywinters@users.noreply.github.com>
2022-05-26 16:04:34 +0000

    update

--- source/lib/react-ex/src/components/flex.js
+++ source/lib/react-ex/src/components/flex.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export function Flex({
     className, //
     style,
     children,
+    onClick,
+
     align = 'center',
     direction,
     dir,
-    onClick,
+
+    ...rest
 }) {
+    assertNoUnknownProps(Flex, rest);
+
     direction = direction || dir || 'row';
     if (direction === 'col') {
         direction = 'column';
@@ -26,5 +32,30 @@ export function Flex({
         >
             {children}
         </div>
+    );
+}
+
+Flex.propTypes = {
+    className: PropTypes.string,
+    style: PropTypes.object,
+    onClick: PropTypes.func,
+
+    align: PropTypes.string,
+    direction: PropTypes.string,
+    dir: PropTypes.string,
+};
+
+function assertNoUnknownProps(Component, rest) {
+    const entries = Object.entries(rest);
+    if (entries.length === 0) {
+        return;
+    }
+    for (let [name, value] of entries) {
+        console.error(`Unknown property '${name}' with value`, value);
+    }
+    throw new Error(
+        `Unknown properties on Component ${Component.name}: ${Object.keys(rest)
+            .map((s) => `'${s}'`)
+            .join(', ')}.  See console for details.`
     );
 }
