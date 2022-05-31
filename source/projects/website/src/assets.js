a4123db60c179f54566386a56de30256c792a51d
Ridley Winters <65878718+ridleywinters@users.noreply.github.com>
2022-05-31 14:55:14 +0000

    update

--- source/projects/website/src/assets.js
+++ source/projects/website/src/assets.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Flex, useCommonStyles, useAsyncEffect, PixelatedImage } from '../../../lib/react-ex';
-import assets from 'glob:$(MONOREPO_ROOT)/source;assets/proto/**/*{.png,.asset.yaml}';
+import assets from 'glob:$(MONOREPO_ROOT)/source;assets/**/*{.asset.yaml,.png,.vox}';
 import * as core from '../../../lib/core';
 
 const assetURL = Object.fromEntries(assets.matches.map(({ url }) => [url.split('/').pop(), url]));
@@ -11,47 +11,138 @@ export default function () {
     useCommonStyles();
 
     return (
-        <div>
+        <div
+            style={{
+                width: '62rem',
+                margin: '1rem auto 6rem',
+            }}
+        >
+            <h1>Raiment Studios</h1>
+            <p>
+                🚧 Under construction: this page will house previews of all the content and assets
+                used in the Raiment Studios projects. As they are all have some form of open
+                license, the goal is to promote reuse and contribution.
+            </p>
+            <h3>Sprites</h3>
+            <Flex style={{ margin: 4 }}>
+                <select value={sortOrder} onChange={(evt) => setSortOrder(evt.target.value)}>
+                    <option>alphabetical</option>
+                    <option>-alphabetical</option>
+                </select>
+            </Flex>
             <div
                 style={{
-                    width: '62rem',
-                    margin: '1rem auto 6rem',
+                    display: 'grid',
+                    gridGap: '15px',
+                    gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
                 }}
             >
-                <h1>Raiment Studios</h1>
-                <p>
-                    🚧 Under construction: this page will house previews of all the content and
-                    assets used in the Raiment Studios projects. As they are all have some form of
-                    open license, the goal is to promote reuse and contribution.
-                </p>
-                <h3>Sprites</h3>
-                <Flex style={{ margin: 4 }}>
-                    <select value={sortOrder} onChange={(evt) => setSortOrder(evt.target.value)}>
-                        <option>alphabetical</option>
-                        <option>-alphabetical</option>
-                    </select>
-                </Flex>
-                <div
-                    style={{
-                        display: 'grid',
-                        gridGap: '15px',
-                        gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
-                    }}
-                >
-                    {Object.entries(assetURL)
-                        .filter(([key]) => key.endsWith('.png'))
-                        .sort(([keyA], [keyB]) => {
-                            if (sortOrder === '-alphabetical') {
-                                return -keyA.localeCompare(keyB);
-                            } else {
-                                return keyA.localeCompare(keyB);
-                            }
-                        })
-                        .map(([key, value]) => (
-                            <ImageInfo key={key} url={value} />
-                        ))}
-                </div>
+                {Object.entries(assetURL)
+                    .filter(([key]) => key.endsWith('.png'))
+                    .sort(([keyA], [keyB]) => {
+                        if (sortOrder === '-alphabetical') {
+                            return -keyA.localeCompare(keyB);
+                        } else {
+                            return keyA.localeCompare(keyB);
+                        }
+                    })
+                    .map(([key, value]) => (
+                        <ImageInfo key={key} url={value} />
+                    ))}
             </div>
+            <h3>VOX Models</h3>
+            <div
+                style={{
+                    display: 'grid',
+                    gridGap: '15px',
+                    gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+                }}
+            >
+                {Object.entries(assetURL)
+                    .filter(([key]) => key.endsWith('.vox'))
+                    .map(([key, value]) => (
+                        <VOXTile key={key} name={key} url={value} />
+                    ))}
+            </div>
+        </div>
+    );
+}
+
+function VOXTile({ name, url }) {
+    const [active, setActive] = React.useState(false);
+
+    return (
+        <Flex
+            dir="col"
+            style={{
+                border: 'solid 1px #CCC',
+                padding: 4,
+                borderRadius: 8,
+                gridColumnStart: active ? 1 : 'inherit',
+                gridColumnEnd: active ? 5 : 'inherit',
+                boxShadow: '2px 2px 2px 2px rgba(0,0,0,0.05)',
+                userSelect: 'none',
+                cursor: 'pointer',
+            }}
+            onClick={() => {
+                setActive(!active);
+            }}
+        >
+            <div>{name}</div>
+            {active && <VOXTileDetails url={url} />}
+        </Flex>
+    );
+}
+
+function VOXTileDetails({ url }) {
+    const [data, setData] = React.useState(null);
+
+    useAsyncEffect(
+        async (token) => {
+            const resp = await fetch(`${url}.asset.yaml`);
+            const text = await resp.text();
+            let data = core.parseYAML(text);
+            token.check();
+            setData(data);
+            console.log(data);
+
+            const resp2 = await fetch(url);
+            const blob = await resp2.blob();
+            data = { ...data };
+            data.filesize = blob.size;
+
+            console.log(url, blob);
+            setData(data);
+        },
+        [url]
+    );
+    return (
+        <div style={{ flex: '1 0 0', width: '100%', marginTop: '1rem' }}>
+            {Object.entries(data || {}).map(([name, value]) => (
+                <Flex align="start" style={{ marginBottom: '0.5rem' }}>
+                    <div style={{ flex: '0 0 0.5rem' }} />
+                    <div style={{ flex: '0 0 6rem' }}>{name}</div>
+                    <div style={{ flex: '1 0 0', whiteSpace: 'pre-wrap' }}>
+                        <code>{typeof value === 'string' ? textToReact(value) : value}</code>
+                    </div>
+                </Flex>
+            ))}
+            <Flex align="start" style={{ marginBottom: '0.5rem' }}>
+                <div style={{ flex: '0 0 0.5rem' }} />
+                <div style={{ flex: '0 0 6rem' }}>Preview</div>
+                <div>
+                    <div
+                        style={{
+                            padding: 96,
+                            border: 'solid 1px #CCC',
+                            borderRadius: 8,
+                        }}
+                    >
+                        3D Preview goes here
+                    </div>
+                    <div>capture screenshot</div>
+                </div>
+            </Flex>
         </div>
     );
 }
@@ -63,8 +154,22 @@ function ImageInfo({ url }) {
     useAsyncEffect(async (token) => {
         const resp = await fetch(`${url}.asset.yaml`);
         const text = await resp.text();
+        const data = core.parseYAML(text);
         token.check();
-        setData(core.parseYAML(text));
+        setData(data);
+
+        const img = await loadImage(url);
+        console.log(img, img.width, img.height);
+        data.attributes = data.attributes || {};
+        const attr = data.attributes;
+        if (attr.width !== img.width) {
+            attr.width = img.width;
+        }
+        if (attr.height !== img.height) {
+            attr.height = img.height;
+        }
+        token.check();
+        setData(data);
     }, []);
 
     return (
@@ -98,10 +203,24 @@ function ImageInfo({ url }) {
                     </div>
                     <h3>License</h3>
                     <pre>{textToReact(data?.license)}</pre>
+                    <h3>Attributes</h3>
+                    <pre>{data && data.attributes && core.stringifyYAML(data.attributes)}</pre>
                 </div>
             )}
         </Flex>
     );
+}
+
+function loadImage(src) {
+    return new Promise((resolve, reject) => {
+        let img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = (err) => {
+            console.error(err);
+            reject(err);
+        };
+        img.src = src;
+    });
 }
 
 function textToReact(s) {
