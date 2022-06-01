8714d88018a099f55d8b8a1a618ed76a7e83f1d0
Ridley Winters <65878718+ridleywinters@users.noreply.github.com>
2022-06-01 00:16:00 +0000

    update

--- source/projects/website/src/vox_preview.js
+++ source/projects/website/src/vox_preview.js
@@ -8,8 +8,8 @@ import {
     BasicLighting,
     GroundPlane,
 } from '../../../lib/engine/src';
-import { chunk } from 'lodash';
 
+// 🚧 TODO: move to core
 class DataReader {
     constructor(arrayBuffer) {
         this._view = new DataView(arrayBuffer);
@@ -22,6 +22,9 @@ class DataReader {
 
     skip(n) {
         this._offset += n;
+    }
+    seek(offset) {
+        this._offset = offset;
     }
 
     readCharString(len) {
@@ -40,11 +43,95 @@ class DataReader {
         return v;
     }
 
+    readInt32LE() {
+        const v = this._view.getInt32(this._offset, true);
+        this._offset += 4;
+        return v;
+    }
+
     readUint32LE() {
         const v = this._view.getUint32(this._offset, true);
         this._offset += 4;
         return v;
     }
+}
+
+function readVOX(arrayBuffer) {
+    const reader = new DataReader(arrayBuffer);
+
+    // https://github.com/ephtracy/voxel-model/blob/master/MagicaVoxel-file-format-vox.txt
+
+    const vox = {
+        header: null,
+        models: [
+            {
+                size: null,
+                voxels: null,
+                palette: null,
+            },
+        ],
+    };
+
+    vox.header = {
+        id: reader.readCharString(4),
+        version: reader.readUint32LE(),
+    };
+
+    const readChunk = () => {
+        const id = reader.readCharString(4);
+        const chunkBytes = reader.readUint32LE();
+        const childBytes = reader.readUint32LE();
+        const chunkEnd = reader.offset + chunkBytes;
+
+        if (id === 'MAIN') {
+            // zero length chunk
+        } else if (id === 'SIZE') {
+            const sizeX = reader.readInt32LE();
+            const sizeY = reader.readInt32LE();
+            const sizeZ = reader.readInt32LE();
+            vox.models[0].size = [sizeX, sizeY, sizeZ];
+        } else if (id === 'XYZI') {
+            const size = reader.readInt32LE();
+            const voxels = new Array(4 * size);
+            for (let i = 0; i < voxels.length; i += 4) {
+                voxels[i + 0] = reader.readUint8();
+                voxels[i + 1] = reader.readUint8();
+                voxels[i + 2] = reader.readUint8();
+                voxels[i + 3] = reader.readUint8();
+            }
+            vox.models[0].voxels = voxels;
+        } else if (id === 'RGBA') {
+            const size = 256;
+            const palette = new Array(4 * size);
+            for (let i = 0; i < palette.length; i += 4) {
+                palette[i + 0] = reader.readUint8();
+                palette[i + 1] = reader.readUint8();
+                palette[i + 2] = reader.readUint8();
+                palette[i + 3] = reader.readUint8();
+            }
+            vox.models[0].palette = palette;
+        } else {
+            console.log('Unhandled', id);
+            reader.skip(chunkBytes);
+        }
+        if (reader.offset !== chunkEnd) {
+            console.log(
+                `${id}: did not read the entire chunk. ${
+                    chunkEnd - reader.offset
+                } bytes unread of ${chunkBytes}.`
+            );
+        }
+        reader.seek(chunkEnd);
+
+        if (childBytes > 0) {
+            const end = reader.offset + childBytes;
+            while (reader.offset < end) {
+                readChunk();
+            }
+        }
+    };
+    readChunk();
+    return vox;
 }
 
 export function VOXPreview({ url }) {
@@ -63,50 +150,8 @@ export function VOXPreview({ url }) {
             const blob = await resp.blob();
 
             const arrayBuffer = await blob.arrayBuffer();
-            const reader = new DataReader(arrayBuffer);
-
-            // https://github.com/ephtracy/voxel-model/blob/master/MagicaVoxel-file-format-vox.txt
-
-            const vox = {
-                header: null,
-                models: [],
-                chunks: [],
-            };
-
-            const header = {
-                id: reader.readCharString(4),
-                version: reader.readUint32LE(),
-            };
-
-            vox.header = header;
-
-            const readChunk = (parent) => {
-                const id = reader.readCharString(4);
-                const chunkBytes = reader.readUint32LE();
-                const childBytes = reader.readUint32LE();
-                reader.skip(chunkBytes);
-
-                console.log(id, chunkBytes, childBytes);
-                const chunk = { id };
-                if (childBytes > 0) {
-                    const end = reader.offset + childBytes;
-                    chunk.chunks = [];
-
-                    readChunkList(chunk, end);
-                }
-
-                parent.chunks.push(chunk);
-            };
-
-            const readChunkList = (parent, end) => {
-                while (reader.offset < end) {
-                    readChunk(parent);
-                }
-            };
-
-            readChunk(vox);
-
-            console.log(vox);
+            const voxModel = readVOX(arrayBuffer);
+            console.log(voxModel);
         },
         [url]
     );
